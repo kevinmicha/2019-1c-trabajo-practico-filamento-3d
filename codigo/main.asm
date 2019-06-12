93d9cfc07b3cc9e48dd2b352c345690ce98e07dd
rarias <riqarias@gmail.com>
2019-06-12 16:45:14 -0300

    horrible!

--- codigo/main.asm
+++ codigo/main.asm
@@ -1,24 +1,24 @@
 ;-------------------------------------------------------------------------
-; AVR - Configuración y transmisión por puerto serie
+; AVR - ConfiguraciÃ³n y transmisiÃ³n por puerto serie
 ; Tiene las rutinas 
 ;	RESET: programa principal para testear las rutinas
 ;	USART_init: configura el puerto serie para tx/rx por interrupciones
-;	ISR_RX_USART_COMPLETA: rutina de servicio de interrupción de recepción
-;	ISR_REG_USART_VACIO: rutina de serv. de int. de transmisión
-;	TEST_TX: rutina de transmisión de prueba
-;	TX_MSJ: rutina de transmisión genérica
+;	ISR_RX_USART_COMPLETA: rutina de servicio de interrupciÃ³n de recepciÃ³n
+;	ISR_REG_USART_VACIO: rutina de serv. de int. de transmisiÃ³n
+;	TEST_TX: rutina de transmisiÃ³n de prueba
+;	TX_MSJ: rutina de transmisiÃ³n genÃ©rica
 ;
-; Ultima actualización: 2019-MAY-05 09:47
+; Ultima actualizaciÃ³n: 2019-MAY-05 09:47
 ;-------------------------------------------------------------------------
 
 ;-------------------------------------------------------------------------
 ; MCU: ATmega8/ATmega328P 
-; Según el entorno de desarrollo, el MCU (MicroController Unit) se elige
+; SegÃºn el entorno de desarrollo, el MCU (MicroController Unit) se elige
 ; a) desde Project->Properties se elige el microcontrolador
 ; b) con una directiva de include.
 ; En ambos casos se termina incluyendo un archivo m8def.inc/m328Pdef.inc
-; que definen un símbolo _M8DEF_INC_/_M328PDEF_INC_ que se usa en toda
-; este código para distinguir entre registros de uno y otro micro.
+; que definen un sÃ­mbolo _M8DEF_INC_/_M328PDEF_INC_ que se usa en toda
+; este cÃ³digo para distinguir entre registros de uno y otro micro.
 ;-------------------------------------------------------------------------
 ; Si el modo de seleccionar el MCU es el b) descomentar el include que 
 ; corresponda:
@@ -31,8 +31,8 @@
 ;-------------------------------------------------------------------------
 ; Puerto donde se conecta un LED
 ;-------------------------------------------------------------------------
-; En una placa Arduino uno el led está en PINB.5 y se enciende con '1'
-; para otros circuitos verificar la ubicación de algún led y definir lo 
+; En una placa Arduino uno el led estÃ¡ en PINB.5 y se enciende con '1'
+; para otros circuitos verificar la ubicaciÃ³n de algÃºn led y definir lo 
 ; siguiente:
 .equ	PORT_LED	= PORTB	; registro del puerto
 .equ	DIR_LED		= DDRB	; registro de direcciones del puerto
@@ -42,8 +42,8 @@
 ;-------------------------------------------------------------------------
 ; MACROS
 ;-------------------------------------------------------------------------
-; Inclusión de macros.  El *.inc debe estar en la misma carpeta que los 
-; demás archivos fuente o bien incluir un path al mismo mediante:
+; InclusiÃ³n de macros.  El *.inc debe estar en la misma carpeta que los 
+; demÃ¡s archivos fuente o bien incluir un path al mismo mediante:
 ;	Project->Properties->Toolchain->AVR Assembler->General->Include Paths
 .include "avr_macros.inc"	
 .listmac					; permite ver las macros en el listado *.lss
@@ -51,8 +51,8 @@
 ;-------------------------------------------------------------------------
 ; CONSTANTES
 ;-------------------------------------------------------------------------
-.equ	BUF_SIZE	= 64	; tamaño en bytes del buffer de transmisión
-.equ	LF	= 13			; '\n' caracter ascii de incremento de línea
+.equ	BUF_SIZE	= 64	; tamaÃ±o en bytes del buffer de transmisiÃ³n
+.equ	LF	= 13			; '\n' caracter ascii de incremento de lÃ­nea
 .equ	CR  = 10			; '\r' caracter ascii de retorno de carro
 .equ	ciclos = 61
 
@@ -60,9 +60,9 @@
 ; variables en SRAM
 ;-------------------------------------------------------------------------
 		.dseg				; Segmento de datos (RAM)
-TX_BUF:	.byte	BUF_SIZE	; buffer de transmisión serie
-RX_BUF: .byte	BUF_SIZE	; buffer de recepción de datos por puerto serie
-VERSION:.byte	1			; nro. de versión del programa
+TX_BUF:	.byte	BUF_SIZE	; buffer de transmisiÃ³n serie
+RX_BUF: .byte	BUF_SIZE	; buffer de recepciÃ³n de datos por puerto serie
+VERSION:.byte	1			; nro. de versiÃ³n del programa
 ADC_B: .byte 3
 
 ;-------------------------------------------------------------------------
@@ -86,7 +86,7 @@ ADC_B: .byte 3
 ; CODIGO
 ;-------------------------------------------------------------------------
 			.cseg
-		rjmp	RESET			; interrupción del reset
+		rjmp	RESET			; interrupciÃ³n del reset
 		
 		.org	INT0addr
 		rjmp	PASO_1SEG
@@ -100,23 +100,23 @@ ADC_B: .byte 3
 		.org	UDREaddr		; USART Data Register Empty
 		rjmp	ISR_REG_USART_VACIO
 
-		.org 	INT_VECTORS_SIZE ; salteo todos los vectores de interrupción
+		.org 	INT_VECTORS_SIZE ; salteo todos los vectores de interrupciÃ³n
 RESET:	
-		ldi		contador, 61
+		ldi		contador, 61 	; LO QUÃ‰? Esto asÃ­ no va
 
 		ldi		r16,LOW(RAMEND)
 		out 	spl,r16
 		ldi 	r16,HIGH(RAMEND)
-		out 	sph,r16			; inicialización del puntero a la pila
+		out 	sph,r16			; inicializaciÃ³n del puntero a la pila
 
 		sbi		DIR_LED, LED	; configuro como salida el puerto para manejar el LED
 		sbi		PORT_LED, LED	; PRENDO el LED
 
 		ldi		t0, 0x13
-		sts		VERSION, t0		; versión actual de este módulo
+		sts		VERSION, t0		; versiÃ³n actual de este mÃ³dulo
 
 		rcall	USART_init		; Configuro el puerto serie para tx/rx a 19.2 kbps
-								; y habilito la interrupción de recepción de datos.
+								; y habilito la interrupciÃ³n de recepciÃ³n de datos.
 
 		lds		R16,	EICRA												;configuro int 0 por flanco ascendente
 		ori		R16,	(1<<ISC01)|(1<<ISC00)
@@ -137,7 +137,7 @@ RESET:
 
 		rcall	INICIALIZAR_TIMER2
 	
-		sei						; habilitación global de todas las interrupciones
+		sei						; habilitaciÃ³n global de todas las interrupciones
 
 		;rcall	TEST_TX			; transmite un mensaje de prueba
 
@@ -146,7 +146,7 @@ RESET:
 MAIN:							; Programa principal (bucle infinito)
 
 		rcall	MONITOREAR_TOV0
-		tst		eventos			; Pasó algo?
+		tst		eventos			; PasÃ³ algo?
 		breq	MAIN			;	nada
 
 		sbrc	eventos, EVENTO_ADC_FIN
@@ -157,9 +157,9 @@ MAIN:							; Programa principal (bucle infinito)
 		rjmp	MAIN
 
 PROCESO_TRAMA_RX:
-		// Los datos recibidos x puerto serie están a partir del dirección RAM RX_BUF
-		// y terminan siempre con el caracter LF.   Además "bytes_recibidos" me dice
-		// cuántos bytes tiene la trama.
+		// Los datos recibidos x puerto serie estÃ¡n a partir del direcciÃ³n RAM RX_BUF
+		// y terminan siempre con el caracter LF.   AdemÃ¡s "bytes_recibidos" me dice
+		// cuÃ¡ntos bytes tiene la trama.
 		cbr		eventos,(1<<EVENTO_RX_SERIE)	; Limpio flag del evento
 		clr		bytes_recibidos					; limpio nro. de bytes recibidos porque no lo uso
 
@@ -167,11 +167,11 @@ PROCESO_TRAMA_RX:
 		cpi		t0, '1'
 		brne	VER_SI_ES_CERO
 
-		sbi		PORT_LED, LED	; Si recibió un '1', prende el LED
+		sbi		PORT_LED, LED	; Si recibiÃ³ un '1', prende el LED
 		rjmp	MAIN
 
 LEER_ADC:
-		; Leen ADC y guardan el valor leído en ADC_B, ADC_B+1 y ADC_B+2
+		; Leen ADC y guardan el valor leÃ­do en ADC_B, ADC_B+1 y ADC_B+2
 		ldiw  Z, (MSJ_DATO<<1)
 		rcall TX_MSJ
 		ret
@@ -208,28 +208,28 @@ USART_init:
 		; Trama: 8 bits de datos, sin paridad y 1 bit de stop, 
 		outi 	UCSR0C,(0<<UPM01)|(0<<UPM00)|(0<<USBS0)|(0<<UCSZ02)|(1<<UCSZ01)|(1<<UCSZ00)
 		; Configura los terminales de TX y RX; y habilita
-		; 	únicamente la int. de recepción
+		; 	Ãºnicamente la int. de recepciÃ³n
 		outi	UCSR0B,(1<<RXCIE0)|(1<<RXEN0)|(1<<TXEN0)|(0<<UDRIE0)
 #else
-		outi	UBRRH,high(BAUD_RATE)	; Velocidad de transmisión
+		outi	UBRRH,high(BAUD_RATE)	; Velocidad de transmisiÃ³n
 		outi	UBRRL,low(BAUD_RATE)
 		outi	UCSRA,(1<<U2X)			; Modo asinc., doble velocidad
 		outi 	UCSRC,(1<<URSEL)|(0<<UPM1)|(0<<UPM0)|(0<<USBS)|(1<<UCSZ1)|(1<<UCSZ0)
 		outi	UCSRB,(1<<RXCIE)|(1<<RXEN)|(1<<TXEN)|(0<<UDRIE)
 #endif
 		movi	ptr_tx_L,LOW(TX_BUF)	; inicializa puntero al 
-		movi	ptr_tx_H,HIGH(TX_BUF)	; buffer de transmisión.
+		movi	ptr_tx_H,HIGH(TX_BUF)	; buffer de transmisiÃ³n.
 	
 		ldiw	X,TX_BUF				; limpia BUF_SIZE posiciones 
-		ldi		t1, BUF_SIZE			; del buffer de transmisión
+		ldi		t1, BUF_SIZE			; del buffer de transmisiÃ³n
 		clr		t0
 loop_limpia:
 		st		X+,t0
 		dec		t1
 		brne	loop_limpia
 					
-		clr		bytes_a_tx		; nada pendiente de transmisión
-		clr		bytes_recibidos	; nada recibido aún.
+		clr		bytes_a_tx		; nada pendiente de transmisiÃ³n
+		clr		bytes_recibidos	; nada recibido aÃºn.
 
 		popw	X
 		pop		t1
@@ -244,7 +244,7 @@ loop_limpia:
 ; Devuelve: nada
 ;-------------------------------------------------------------------------
 ISR_RX_USART_COMPLETA:
-; EL registro UDR tiene un dato y debería ser procesado
+; EL registro UDR tiene un dato y deberÃ­a ser procesado
 		push	t0
 		pushi	SREG
 		pushw	Y
@@ -266,9 +266,9 @@ ISR_RX_USART_COMPLETA:
 		cp		bytes_recibidos, t0
 		brlo	BUF_RX_CON_ESPACIO
 
-		clr		bytes_recibidos		; error, se sobrepasó el espacio disponible 
+		clr		bytes_recibidos		; error, se sobrepasÃ³ el espacio disponible 
 									; para mensajes recibidos x puerto serie.
-									; Debería informar al main (pero no lo hago).
+									; DeberÃ­a informar al main (pero no lo hago).
 BUF_RX_CON_ESPACIO:		
 		ld		t0, Y
 ACA:
@@ -286,49 +286,49 @@ FIN_ISR_RX_USART:
 
 ;------------------------------------------------------------------------
 ; TRANSMISION: interrumpe cada vez que puede transmitir un byte.
-; Se transmiten "bytes_a_tx" comenzando desde la posición TX_BUF del
-; buffer. Si "bytes_a_tx" llega a cero, se deshabilita la interrupción.
+; Se transmiten "bytes_a_tx" comenzando desde la posiciÃ³n TX_BUF del
+; buffer. Si "bytes_a_tx" llega a cero, se deshabilita la interrupciÃ³n.
 ;
 ; Recibe: 	bytes_a_tx.
 ; Devuelve: ptr_tx_H:ptr_tx_L, y bytes_a_tx.
 ;------------------------------------------------------------------------
-ISR_REG_USART_VACIO:		; UDR está vacío
+ISR_REG_USART_VACIO:		; UDR estÃ¡ vacÃ­o
 		push	t0
 		push	t1
 		pushi	SREG
 		pushw	X
 
 
-		tst		bytes_a_tx	; hay datos pendientes de transmisión?
+		tst		bytes_a_tx	; hay datos pendientes de transmisiÃ³n?
 		breq	FIN_TRANSMISION
 
-		movw	XL,ptr_tx_L	; Recupera puntero al próximo byte a tx.
+		movw	XL,ptr_tx_L	; Recupera puntero al prÃ³ximo byte a tx.
 		ld		t0,X+		; lee byte del buffer y apunta al
 
 #ifdef  _M328PDEF_INC_
 		output	UDR0, t0
 #else
-		output	UDR, t0		; sgte. dato a transmitir (en la próxima int.)
+		output	UDR, t0		; sgte. dato a transmitir (en la prÃ³xima int.)
 #endif
 
 		cpi		XL,LOW(TX_BUF+BUF_SIZE)
 		brlo	SALVA_PTR_TX
 		cpi		XH,HIGH(TX_BUF+BUF_SIZE)
 		brlo	SALVA_PTR_TX
-		ldiw	X,TX_BUF	; ptr_tx=ptr_tx+1, (módulo BUF_SIZE)
+		ldiw	X,TX_BUF	; ptr_tx=ptr_tx+1, (mÃ³dulo BUF_SIZE)
 
 SALVA_PTR_TX:
 		movw	ptr_tx_L,XL	; preserva puntero a sgte. dato
 
 		dec		bytes_a_tx	; Descuenta el nro. de bytes a tx. en 1
 		brne	SIGUE_TX	; si quedan datos que transmitir
-							;	vuelve en la próxima int.
+							;	vuelve en la prÃ³xima int.
 
 FIN_TRANSMISION:			; si no hay nada que enviar,
 #ifdef  _M328PDEF_INC_
 		cbix	UCSR0B, UDRIE0
 #else
-		cbix	UCSRB,	UDRIE	; 	se deshabilita la interrupción.
+		cbix	UCSRB,	UDRIE	; 	se deshabilita la interrupciÃ³n.
 #endif
 
 sigue_tx:
@@ -340,7 +340,7 @@ sigue_tx:
 
 ;-------------------------------------------------------------------------
 ; TEST_TX: transmite el mensaje almacenado en memoria flash a partir
-; de la dirección MSJ_TEST_TX que termina con 0x00 (el 0 no se transmite).
+; de la direcciÃ³n MSJ_TEST_TX que termina con 0x00 (el 0 no se transmite).
 ; Recibe: nada
 ; Devuelve: nada
 ;-------------------------------------------------------------------------
@@ -361,32 +361,32 @@ MSJ_DATO:
 MSJ_TEST_TX:
 ;.db	"Puerto Serie Version 0.3 ",'\r','\n',0
 .db		"Filamento proyecto %"
-.dw		VERSION		; dirección RAM de la variable (byte hexa) a tx
+.dw		VERSION		; direcciÃ³n RAM de la variable (byte hexa) a tx
 .db		'\r','\n',0,0	; el 2do cero completa un nro. par de bytes
 
 ;-------------------------------------------------------------------------
 ; TX_MSJ: transmite el mensaje almacenado en memoria flash a partir
-; de la dirección que se pase en el puntero Z.   El mensaje debe termina 
+; de la direcciÃ³n que se pase en el puntero Z.   El mensaje debe termina 
 ; con 0x00 (el 0 no se transmite).
 ; 
 ; Recibe: Z (=r31|r30)
 ; Devuelve: bytes_a_tx > 0
-; Habilita la int. de transmisión serie con ISR en ISR_REG_USART_VACIO().
+; Habilita la int. de transmisiÃ³n serie con ISR en ISR_REG_USART_VACIO().
 ;-------------------------------------------------------------------------
 TX_MSJ:
 			push	t0
 			pushi	SREG
 			pushw	X
 
-			movw	XL, ptr_tx_L	; toma el último valor del puntero
+			movw	XL, ptr_tx_L	; toma el Ãºltimo valor del puntero
 
 COPIA_A_TX_BUF:
 			lpm		t0,Z+			; y copia de flash a ram
 			tst		t0				; si encuentra un 0x00 en el mensaje, termina
-			breq	ACTIVA_TX_MSJ	;	de cargar el buffer en RAM e incia la transmisión.
+			breq	ACTIVA_TX_MSJ	;	de cargar el buffer en RAM e incia la transmisiÃ³n.
 
 ; Si el carcter es '%' seguido de un nro. hexadecimal de 16 bits, toma el byte
-; de esa dirección de RAM, lo convierte a ASCII y lo pone en el buffer de transmisión.
+; de esa direcciÃ³n de RAM, lo convierte a ASCII y lo pone en el buffer de transmisiÃ³n.
 			cpi		t0, '%'
 			brne	NO_HAY_VARIABLES
 
@@ -405,7 +405,7 @@ COPIA_A_TX_BUF:
 			brlo	NO_HAY_VARIABLES
 			cpi		XH, high(TX_BUF+BUF_SIZE)
 			brlo	NO_HAY_VARIABLES
-			ldiw	X, TX_BUF		; ptr_tx++ módulo BUF_SIZE
+			ldiw	X, TX_BUF		; ptr_tx++ mÃ³dulo BUF_SIZE
 
 NO_HAY_VARIABLES:
 			st		X+,t0
@@ -415,7 +415,7 @@ NO_HAY_VARIABLES:
 			brlo	COPIA_A_TX_BUF
 			cpi		XH, high(TX_BUF+BUF_SIZE)
 			brlo	COPIA_A_TX_BUF
-			ldiw	X, TX_BUF		; ptr_tx++ módulo BUF_SIZE
+			ldiw	X, TX_BUF		; ptr_tx++ mÃ³dulo BUF_SIZE
 
 			rjmp	COPIA_A_TX_BUF
 	
@@ -757,5 +757,5 @@ AGAIN:
 	BRNE	AGAIN
 	RET
 ;-------------------------------------------------------------------------
-; fin del código
+; fin del cÃ³digo
 ;-------------------------------------------------------------------------;
